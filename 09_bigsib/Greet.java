091c68ac55d959686e0a3503df13ecd838a4d010
JefferyTang23 <jtang30@stuy.edu>
2021-10-01 12:11:49 -0400

    09

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,20 @@
+//Jeffery Tang
+//APCS
+//HW09 -- Big Sibs Greeting
+//2021-10-1
+
+//DISCOVERIES:
+//
+//UNRESOLVED QUESTIONS:
+//
+
+public class Greet{
+	public static void main (String[] args) {
+		greet("Flim");
+		greet("Flam");
+		greet("Kazaam");
+	}
+	public static void greet(String x) {
+		System.out.println("Hey " + x + ", how are you doing today?");
+	}
+}

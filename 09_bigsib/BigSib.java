15376ea45eaf70be63c9cc6d50aad89fb027a110
SonicEclipsed <soniceclipsed@gmail.com>
2021-10-02 14:29:39 -0400

    BigSib.java

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,10 @@
+//Roaring 20s: Jeffery Tang (Mathias), Jefford Shau (Dylan), Nada Hameed (Ray)
+//APCS
+//HW09 -- Big Sibs Greeting
+//2021-10-2
+
+public class BigSib {
+  public static void greet(String x) {
+		System.out.println("Hey " + x + ", how are you doing today?");
+	}
+}
--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,20 +1,18 @@
-//Jeffery Tang
+//Roaring 20s: Jeffery Tang (Mathias), Jefford Shau (Dylan), Nada Hameed (Ray)
 //APCS
 //HW09 -- Big Sibs Greeting
-//2021-10-1
+//2021-10-2
 
 //DISCOVERIES:
-//
+//Using the command "javac" on Greet.java creates the file "BigSib.class"
+//Using "java Greet" after deleting "BigSib.class" returns an error message
 //UNRESOLVED QUESTIONS:
-//
+//Is a java file able to call a method from another java file that calls a method from yet another java file?
 
 public class Greet{
 	public static void main (String[] args) {
-		greet("Flim");
-		greet("Flam");
-		greet("Kazaam");
-	}
-	public static void greet(String x) {
-		System.out.println("Hey " + x + ", how are you doing today?");
+		BigSib.greet("Flim");
+    BigSib.greet("Flam");
+    BigSib.greet("Kazaam");
 	}
 }

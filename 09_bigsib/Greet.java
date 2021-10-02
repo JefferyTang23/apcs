cbc5043fabac7e44e87e31dcd5a9e9e373701618
SonicEclipsed <soniceclipsed@gmail.com>
2021-10-02 14:32:55 -0400

    fixed indentations

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -12,7 +12,7 @@
 public class Greet{
 	public static void main (String[] args) {
 		BigSib.greet("Flim");
-    BigSib.greet("Flam");
-    BigSib.greet("Kazaam");
+    	BigSib.greet("Flam");
+    	BigSib.greet("Kazaam");
 	}
 }

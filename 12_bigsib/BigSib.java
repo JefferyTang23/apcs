f31e6e12bcbf52c8bedbe3f93fc8edebf8a0f37c
SonicEclipsed <soniceclipsed@gmail.com>
2021-10-05 20:11:53 -0400

    HW12

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,20 @@
+//Triple J: Jeffery Tang (Mathias), Jefford Shau (Dylan), Anthony Sun (Corn)
+//APCS
+//HW12 -- Greetings with Hello Messages
+//2021-10-5
+
+//DISCO:
+//Creating the BigSib object "richard" allows methods from BigSib.java to be used using richard.<method>
+//QCC:
+//What exactly does creating the BigSib object "richard" allow us to do that can't be done with BigSib.<method>?
+//What are instance variables and how are they used?
+
+public class BigSib {
+	String HelloMsg;
+	public String greet(String x) {
+		return ( HelloMsg + " " + x);
+	}
+	public void setHelloMsg(String n) {
+		HelloMsg = n;
+	}
+}
---
+++ 12_bigsib/Greet.java
@@ -0,0 +1,33 @@
+//Triple J: Jeffery Tang (Mathias), Jefford Shau (Dylan), Anthony Sun (Corn)
+//APCS
+//HW12 -- Greetings with Hello Messages
+//2021-10-5
+
+//DISCO:
+//Creating the BigSib object "richard" allows methods from BigSib.java to be used using richard.<method>
+//QCC:
+//What exactly does creating the BigSib object "richard" allow us to do that can't be done with BigSib.<method>?
+//What are instance variables and how are they used?
+
+public class Greet {
+    public static void main( String[] args ){
+        String greeting;
+        BigSib richard = new BigSib();
+
+        richard.setHelloMsg("Word up");
+        greeting = richard.greet("freshman");
+        System.out.println(greeting);
+
+        richard.setHelloMsg("Salutations");
+        greeting = richard.greet("Dr. Spaceman");
+        System.out.println(greeting);
+
+        richard.setHelloMsg("Hey ya");
+        greeting = richard.greet("Kong Fooey");
+        System.out.println(greeting);
+
+        richard.setHelloMsg("Sup");
+        greeting = richard.greet("mom");
+        System.out.println(greeting);
+    }
+}

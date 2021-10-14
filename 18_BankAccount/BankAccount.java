6a09b5e02586afcaed2c2c8231cd7ebb5b82d022
Jeffery Tang <78169529+JefferyTang23@users.noreply.github.com>
2021-10-14 12:11:04 -0400

    Update BankAccount.java

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -1,1 +1,31 @@
 
+public class BankAccount {
+  public String holderName;
+  private String accountPassword;
+  private int pin;
+  public int accountNumber;
+  private double accBalance;
+  private void withdraw (double withdrawAmount) {
+    accBalance = accBalance - withdrawAmount;
+    }
+  private void deposit (double depositAmount) {
+    accBalance = accBalance + depositAmount;
+    }
+  private void setHolderName (String name) {
+    holderName = name;
+    }
+  private void setAccountNumber (int accNum) {
+    accountNumber = accNum;
+    }
+  private void setAccountPassword (String accPW) {
+    accountPassword = accPW;
+    }
+  private void setPin (int accPIN) {
+    pin = accPIN;
+    }
+  private void printInfo () {
+    System.out.println (holderName);
+    System.out.println (accountNumber);
+    System.out.println (accBalance);
+    }
+}

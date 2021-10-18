1fbea1ca05bb6585fd70c1778d3b4988fa535191
Jeffery Tang <78169529+JefferyTang23@users.noreply.github.com>
2021-10-17 20:08:21 -0400

    Update BankAccount.java

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -1,36 +1,45 @@
 /*
 DISCO: 
 QCC: 
-Team NIWWD's Latest and Greatest Q2 Response: 
-Team NIWWD's Latest and Greatest Q3 Response: 
+Team NIWWD's Latest and Greatest Q2 Response: We know that Java provides a default contructor before we wrote our own constructor because 
 */
 
-public class BankAccount {
-  public static String holderName;
-  private static String accountPassword;
-  private static int pin;
-  public static int accountNumber;
-  private static double accBalance;
+//Team NIWWD's Latest and Greatest Q3 Response: 
 
-  private static void withdraw (double withdrawAmount) {
+public class BankAccount {
+  public String holderName;
+  private String accountPassword;
+  private int pin;
+  public int accountNumber;
+  private double accBalance;
+  
+  public BankAccount (String hName, String accPass, int fourdPIN, int accNumb, double accBal) {
+    holderName = hName;
+    accountPassword = accPass;
+    pin = fourdPIN;
+    accountNumber = accNumb;
+    accBalance = accBal;
+  }
+
+  private void withdraw (double withdrawAmount) {
     accBalance = accBalance - withdrawAmount;
     }
-  private static void deposit (double depositAmount) {
+  private void deposit (double depositAmount) {
     accBalance = accBalance + depositAmount;
     }
-  private static void setHolderName (String name) {
+  private void setHolderName (String name) {
     holderName = name;
     }
-  private static void setAccountNumber (int accNum) {
+  private void setAccountNumber (int accNum) {
     accountNumber = accNum;
     }
-  private static void setAccountPassword (String accPW) {
+  private void setAccountPassword (String accPW) {
     accountPassword = accPW;
     }
-  private static void setPin (int accPIN) {
+  private void setPin (int accPIN) {
     pin = accPIN;
     }
-  private static void printInfo () {
+  private void printInfo () {
     System.out.println (holderName);
     System.out.println (accountNumber);
     System.out.println (accBalance);

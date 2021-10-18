4c0860be497ef2fab03c3e6d8d96938d739fc07a
SonicEclipsed <soniceclipsed@gmail.com>
2021-10-18 19:26:48 -0400

    heading

---
+++ 19_BankAccount/BankAccount.java
@@ -0,0 +1,92 @@
+/**
+   NIWWD: Kevin Xiao + Mr. Swag, Jeffery Tang + Mathias, Kaitlin Ho + Apple 
+   APCS
+   HW19 -- Better BankAccount in Java
+   2021-10-19
+**/
+
+/**
+   DISCO: 
+   QCC: 
+**/
+
+public class BankAccount {
+
+  //instance variables
+  private String name;
+  private String passwd;
+  private short pin;
+  private int acctNum;
+  private double balance;
+  //---------------------------------------------
+
+
+  //mutators
+  public String setName( String newName ) {
+    String oldName = name;
+    name = newName;
+    return oldName;
+  }
+
+  public String setPasswd( String newPasswd ) {
+    String oldPasswd = passwd;
+    passwd = newPasswd;
+    return oldPasswd;
+  }
+
+  public short setPin( short newPin ) {
+    short oldPin = pin;
+    pin = newPin;
+    return oldPin;
+  }
+
+  public int setAcctNum( int newAcctNum ) {
+    int oldAcctNum = acctNum;
+    acctNum = newAcctNum;
+    return oldAcctNum;
+  }
+
+  public double setBalance( double newBalance ) {
+    double oldBalance = balance;
+    balance = newBalance;
+    return oldBalance;
+  }
+  //---------------------------------------------
+
+
+  public void deposit( double depositAmount ) {
+    balance = balance + depositAmount;
+  }
+
+  public void withdraw( double withdrawAmount ) {
+    balance = balance - withdrawAmount;
+  }
+
+
+  //overwritten toString()
+  public String toString() {
+    String retStr = "\nAccount info:\n=======================";
+    retStr = retStr + "\nName: " + name;
+    retStr = retStr + "\nPassword: " + passwd;
+    retStr = retStr + "\nPIN: " + pin;
+    retStr = retStr + "\nAccount Number: " + acctNum;
+    retStr = retStr + "\nBalance: " + balance;
+    retStr = retStr + "\n=======================";
+    return retStr;
+  }
+
+
+  //main method for testing
+  public static void main( String[] args ) {
+    BankAccount ba = new BankAccount();
+    ba.setName("John");
+    ba.setPasswd("hellotheremyfriends");
+    ba.setPin((short) 2131);
+    ba.setAcctNum(123456789);
+    ba.setBalance(1000.00);
+    ba.deposit(500.0);
+    ba.withdraw(241.0);
+    System.out.println(ba.toString());
+  }
+
+}

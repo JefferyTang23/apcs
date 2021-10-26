94a55431157f4d8c55b1204c52b0b4b0a40d59ad
SonicEclipsed <soniceclipsed@gmail.com>
2021-10-26 17:53:06 -0400

    init

---
+++ 25_repeat/Repeater.java
@@ -0,0 +1,39 @@
+/**
+   NIWWD: Kevin Xiao + Mr. Swag, Jeffery Tang + Mathias, Kaitlin Ho + Apple 
+   APCS
+   HW25 -- Repeater
+   2021-10-26
+**/
+
+/**
+   DISCO: 
+   QCC: 
+**/
+
+public class Repeater {
+  public static String fenceW(int numPosts) {
+    String fence = "|";
+    int counter = 1;
+    while (counter < numPosts) {
+      fence += "--|";
+      counter += 1;
+    }
+    return fence;
+  }
+  public static String fenceR(int numPosts) {
+    String fence = "|";
+    if (1 < numPosts) {
+      fence += "--|";
+      fenceR(numPosts-1);
+    }
+    return fence;
+  }
+  public static void main( String[] args ) {
+    System.out.println(fenceW(1));
+    System.out.println(fenceW(2));
+    System.out.println(fenceW(3));
+    System.out.println(fenceR(1));
+    System.out.println(fenceR(2));
+    System.out.println(fenceR(3));
+  }
+}

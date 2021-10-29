66ec94b5cf59c5ae96c95c27d9946e69b5905b2a
Jeffery Tang <78169529+JefferyTang23@users.noreply.github.com>
2021-10-28 20:56:32 -0400

    Update Rof.java

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -1,21 +1,23 @@
-/**
-   NIWWD: Hugo Jenkins + <duck name>, Jeffery Tang + Mathias, Melody Lew + <duck name>
+/*
+   NIWWD: Hugo Jenkins + Boary>, Jeffery Tang + Mathias, Melody Lew + Ollie
    APCS
    HW27 -- FOR the Love of Strings
    2021-10-28
-   time spent: 0.8
-**/
+   time spent: 0.8 hrs
+*/
 
-/**
-   DISCO:
-   
-   QCC:
-   
-**/
-
+/*
+DISCO:
+0. length is equivalent to the number of 16-bit Unicode of the string.
+1. substring can be used to return part of a string, or a substring of a string. The first input is at which index the substring begins,
+the second input is at which index the substring ends.
+QCC:
+0. What happens if the first input of the substring is a negative integer? 
+1. Likewise, what happens if the second input of the substring is greater than the length of the string?
+*/
 
 public class Rof {
- 
+
   public static String FenceF(int posts) {
      String f = "|";
      for (int i = 1 ; i < posts ; i++) {
@@ -25,14 +27,14 @@ public class Rof {
   }
   public static String reverseF(String s) {
      String r = "";
-     for (int i = 1 ; i < s.length() ; i++) {
+     for (int i = 1 ; i - 1< s.length() ; i++) {
         r += s.substring(s.length() - i, s.length() - i + 1);
      }
      return r;
   }
   public static String reverseR(String s) {
    String r = "";
-   if (s.length() > 1) {
+   if (s.length() >= 1) {
       r += s.substring(s.length() - 1, s.length()) + reverseR(s.substring(0, s.length() - 1));
    }
    return r;
@@ -43,9 +45,13 @@ public class Rof {
     System.out.println(FenceF(1));
     System.out.println(FenceF(2));
     System.out.println(FenceF(3));
-    System.out.println("ReverseF test:");
+    System.out.println("ReverseF tests:");
     System.out.println(reverseF("stressed"));
-    System.out.println("ReverseR test:");
+    System.out.println(reverseF("g"));
+    System.out.println(reverseF("gy"));
+    System.out.println("ReverseR tests:");
     System.out.println(reverseR("stressed"));
+    System.out.println(reverseR("g"));
+    System.out.println(reverseF("gy"));
   }
 }

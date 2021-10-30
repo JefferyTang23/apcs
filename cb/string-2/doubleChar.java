269733308df3c48c41f78a39197ee96bb880c927
SonicEclipsed <soniceclipsed@gmail.com>
2021-10-29 22:19:58 -0400

    string-2 code

--- cb/string-2/doubleChar.java
+++
@@ -1,7 +0,0 @@
-public String doubleChar(String str) {
-  String dc = "";
-  for (int i = 0 ; i < str.length() ; i++) {
-    dc += str.substring(i, i + 1) + str.substring(i, i + 1);
-  }
-  return dc;
-}
--- cb/string-2/endOther.java
+++
@@ -1,8 +0,0 @@
-public boolean endOther(String a, String b) {
-  int al = a.length();
-  int bl = b.length();
-  if (al > bl) {
-    return a.substring(al - bl) == b;
-  }
-
-}

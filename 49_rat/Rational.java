906f275829b0501c8a1c1088eb75a050361624e9
Jeffery Tang <78169529+JefferyTang23@users.noreply.github.com>
2021-12-22 21:02:34 -0500

    Update Rational.java

--- 49_rat/Rational.java
+++ 49_rat/Rational.java
@@ -6,21 +6,18 @@ HW49 -- Java Conventions
 time spent:  0.5 hours
 
 DISCO
--
+- We can use "throw new ClassCastException()" to throw an error. 
+- We should use boolean short circuiting to get out if the object is not of class Rational. 
 
 QCC
--
+- Do we have to implement Comparable?
  *****************************************************/
 
-public class Rational
+public class Rational implements Comparable
 {
-  //   Instance variables for numerator and denominator
   private int _numerator;
   private int _denominator;
 
-
-  // Default constructor (no parameters)
-  // creates a new Rational with value 0/1
   public Rational()
   {
     _numerator = 0;
@@ -28,9 +25,6 @@ public class Rational
   }
 
 
-  // Constructor
-  // takes 2 parameters, one for the numerator, one for the denominator
-  // if an invalid denominator is attempted, should print a message and set the number to 0/1
   public Rational( int n, int d )
   {
     this();
@@ -45,28 +39,18 @@ public class Rational
   }
 
 
-  // toString
-  // returns a string representation of the rational number (formatting of your choice)
   public String toString()
   {
     return _numerator + " / " + _denominator;
   }
 
 
-  // floatValue
-  // returns floating point approximation of the number
-  // uses the most precise floating point primitive
   public double floatValue()
   {
     return (double)_numerator / _denominator;
   }
 
 
-  // multiply
-  // takes a Rational parameter and multiplies it by this Rational
-  // does not return a value
-  // modifies this object, leaves parameter alone
-  // need not reduce
   public void multiply( Rational other )
   {
     _numerator   = this._numerator   * other._numerator;
@@ -74,8 +58,6 @@ public class Rational
   }
 
 
-  // divide
-  // works the same as multiply, except the operation is division
   public void divide( Rational other )
   {
     if ( other._numerator != 0 ) {
@@ -129,61 +111,25 @@ public class Rational
     _numerator = this._numerator / gcdr;
     _denominator = this._denominator / gcdr;
   }
+  
+  public int compareTo(Object other){
 
-  public int compareTo(Object r) {
-    Rational s = (Rational) r;
-    this.reduce();
-    s.reduce();
-
-    if (this._numerator * s._denominator > s._numerator * this._denominator) {
-      return 1;
-    } else if (this._numerator * s._denominator < s._numerator * this._denominator) {
-      return -1;
-    } else {
-      return 0;
+    if(other instanceof Rational){
+      Rational holder = (Rational) other;
+      return this._numerator * holder._denominator - holder._numerator * this._denominator;
     }
+    throw new ClassCastException("\ncompareTo() input not a Rational");
 
   }
 
+  public boolean equals(Object other){
 
-  //main method for testing
-  public static void main( String[] args )
-  {
-      Rational r = new Rational(3, 7 );
-      Rational s = new Rational();
-      Rational t = new Rational( 8, 5 );
-
-      Rational u = new Rational( 1, 2 );
-      Rational v = new Rational( 1, 6 );
-      Rational w = new Rational( 8, 12 );
-
-      System.out.println((r) instanceof Rational);
-      System.out.println("\nTESTING compareTo:");
-      System.out.println(w.compareTo(v)); // should be 1
-      System.out.println(v.compareTo(w)); // should be -1
-      System.out.println(u.compareTo(u)); // should be 0
-
-      System.out.println("\nTESTING ADD:");
-      r.add(t);
-      System.out.println(r); // should be 71/35
-      System.out.println(r.gcd()); // Should be 1
-      w.add(u);
-      System.out.println(w); // should be 7/6
-      System.out.println(w.gcd()); // Should be 1
-      s.add(v);
-      System.out.println(s); // should be 1/6
-      System.out.println(s.gcd()); // should be 1
-
-      System.out.println("\nTESTING SUBTRACT:");
-      u.subtract(v);
-      System.out.println(u); // should be 4/12
-      w.subtract(s);
-      System.out.println(w); // should be 36/36
-
-      System.out.println("\nTESTING REDUCE:");
-      u.reduce();
-      System.out.println(u); // should be 1/3
-      w.reduce();
-      System.out.println(w); // should be 1/1
+    if(other instanceof Rational){
+      Rational holder = (Rational) other;
+      return this._numerator * holder._denominator == holder._numerator * this._denominator;
+    }
+    return false;
   }
+
+
 }//end class

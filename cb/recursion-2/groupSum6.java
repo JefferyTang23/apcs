5dea22fa4fef7b4309339e68cf1b1a84f7356634
JefferyTang23 <jtang30@stuy.edu>
2022-02-14 22:09:40 -0500

    heading

--- cb/recursion-2/groupSum6.java
+++ cb/recursion-2/groupSum6.java
@@ -1,4 +1,12 @@
 /*
+Jacob Ng + Jeffery Tang
+APCS
+HW64 -- Revisitation
+2022-02-14
+time spent: .6 hr
+*/
+
+/*
 Given an array of ints, is it possible to choose a group of some of the ints, beginning at the start index, such that the group sums to the given target? However, with the additional constraint that all 6's must be chosen. (No loops needed.)
 
 

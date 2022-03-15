edcd58c9b1f9efccee30067bc57029d239787a7e
JefferyTang23 <jtang30@stuy.edu>
2022-03-14 22:11:52 -0400

    hw76

---
+++ 76_llist/List.java
@@ -0,0 +1,30 @@
+// JAB (Jeffery Tang, Andrew Piatetsky, Brian Kang)
+// APCS pd6
+// HW75: Nodal Recall
+// 2022-03-013
+// time spent:  30mins
+
+/***
+ * interface List
+ * subset of actual Java List interface
+ * Specifies actions a List must be able to perform.
+ *
+ * If placed in current working dir, will be consulted instead of
+ *  built-in Java library interface.
+ **/
+
+public interface List
+{
+  //add node to list, containing input String as its data
+  public boolean add( String x );
+
+  //return data in element at position i
+  public String get( int i );
+
+  //overwrite data in element at position i
+  public String set( int i, String x );
+
+  //return length of list
+  public int size();
+
+}//end interface
